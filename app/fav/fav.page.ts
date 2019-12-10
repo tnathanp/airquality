50b7162e727a4c4c7ff409812b4031d7f48f988e
Tanathan Pansuwan <tnathanp@Tanathans-MacBook-Pro.local>
2019-12-10 14:14:34 +0700

    Initialization

---
+++ app/fav/fav.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fav',
+  templateUrl: 'fav.page.html',
+  styleUrls: ['fav.page.scss']
+})
+export class FavPage {
+
+  constructor() {}
+
+}
---
+++ app/tabs/tabs-routing.module.ts
@@ -0,0 +1,58 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  {
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: 'home',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../home/home.module').then(m => m.HomePageModule)
+          }
+        ]
+      },
+      {
+        path: 'fav',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../fav/fav.module').then(m => m.FavPageModule)
+          }
+        ]
+      },
+      {
+        path: 'user',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../user/user.module').then(m => m.UserPageModule)
+          }
+        ]
+      },
+      {
+        path: '',
+        redirectTo: '/tabs/home',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/tabs/home',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class TabsPageRoutingModule {}
