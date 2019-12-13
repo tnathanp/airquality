5ddad7c7abc4999c81f61dc02caa5bc44891e772
Kornkanok Nueangthodthet <Jelly@Kornkanoks-MacBook-Pro-10.local>
2019-12-13 17:00:56 +0700

    first commit ja

--- app/home/home.page.ts
+++ app/home/home.page.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss']
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  ngOnInit(){
+  }
 
-  constructor() {}
+  constructor(public httpClient: HttpClient) {
+  }
+
+  data: Observable<any>;
+
+  Search(ev){
+    this.data=this.httpClient.get("https://website-api.airvisual.com/v1/search?q="+ev.target.value+"&units.temperature=celsius&units.distance=kilometer&AQI=US&language=en");
+    this.data.subscribe(
+        resp=>{ console.log(resp);
+      
+    }
+    )
+  }
+
+  eiei(val: string){
+    console.log(val);
+  }
 
 }
