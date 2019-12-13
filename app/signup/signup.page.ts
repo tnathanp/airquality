d5414cc24b623f6ba519fca0f3b9905c9898d1dc
Tanathan Pansuwan <tnathanp@Tanathans-MacBook-Pro.local>
2019-12-13 08:27:51 +0700

    Signup page api call

--- app/signup/signup.page.ts
+++ app/signup/signup.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupPage implements OnInit {
 
-  constructor() { }
-
   ngOnInit() {
   }
 
+  private toast: ToastController
+  return: Observable<any>;
+
+  constructor(public httpClient: HttpClient) {
+  }
+
+  register(usr: string, pwd: string){
+    this.return = this.httpClient.get("http://airqualityapi.herokuapp.com/register?username="+usr+"&password="+pwd);
+    this.return.subscribe(
+      resp => {
+        console.log(resp.message);
+      }
+    )
+  }
+
+  async showError() {
+    const t = await this.toast.create({
+      message: "This username is already registered",
+      duration: 3000,
+      position: 'bottom'
+    });
+    await t.present();
+  }
 }
