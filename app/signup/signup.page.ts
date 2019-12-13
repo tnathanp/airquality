d0e18d10e7964ea00dff34065b4bd0a297064560
Tanathan Pansuwan <tnathanp@Tanathans-MacBook-Pro.local>
2019-12-13 10:49:37 +0700

    fix signup condition

--- app/signup/signup.page.ts
+++ app/signup/signup.page.ts
@@ -22,26 +22,30 @@ export class SignupPage implements OnInit {
   }
 
   register(usr: string, pwd: string) {
-    this.isSending = true;
-    this.return = this.httpClient.get("http://airqualityapi.herokuapp.com/register?username=" + usr + "&password=" + pwd);
-    this.return.subscribe(
-      resp => {
-        if (resp.message == "Success") {
-          this.navCtrl.navigateBack('/tabs/user');
-          this.showMessage("Success, Please log in","top");
-          this.isSending = false;
-        } else if (resp.message == "Already registered") {
-          this.showMessage("This username is already registered","bottom");
-          this.isSending = false;
-        } else {
-          this.showMessage("Something went wrong! Please try again","bottom");
-          this.isSending = false;
+    if (usr.length >= 3 && pwd.length >= 3) {
+      this.isSending = true;
+      this.return = this.httpClient.get("http://airqualityapi.herokuapp.com/register?username=" + usr + "&password=" + pwd);
+      this.return.subscribe(
+        resp => {
+          if (resp.message == "Success") {
+            this.navCtrl.navigateBack('/tabs/user');
+            this.showMessage("Success, Please log in", "top");
+            this.isSending = false;
+          } else if (resp.message == "Already registered") {
+            this.showMessage("This username is already registered", "bottom");
+            this.isSending = false;
+          } else {
+            this.showMessage("Something went wrong! Please try again", "bottom");
+            this.isSending = false;
+          }
         }
-      }
-    )
+      )
+    } else {
+      this.showMessage("Please put at least 3 characters", "bottom");
+    }
   }
 
-  async showMessage(text: string,pos: any) {
+  async showMessage(text: string, pos: any) {
     const t = await this.toast.create({
       message: text,
       duration: 3000,
