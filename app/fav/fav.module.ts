15b543597a00dc95ac892b530d44b0e167e2d94d
Tanathan Pansuwan <tnathanp@Tanathans-MacBook-Pro.local>
2019-12-14 12:27:26 +0700

    fav page and animation

--- app/fav/fav.module.ts
+++ app/fav/fav.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { FavPage } from './fav.page';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
+    HttpClientModule,
     RouterModule.forChild([{ path: '', component: FavPage }])
   ],
   declarations: [FavPage]
--- app/fav/fav.page.ts
+++ app/fav/fav.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-fav',
@@ -7,5 +9,55 @@ import { Component } from '@angular/core';
 })
 export class FavPage {
 
-  constructor() {}
+  isLogin: boolean = false;
+  isAdded: boolean = false;
+  isLoading: boolean = true;
+  account: string = '';
+  level: string = 'default';
+  aqi: number = 0;
+
+  constructor(public httpClient: HttpClient) { }
+
+  return: Observable<any>;
+  fetch: Observable<any>;
+
+  ionViewWillEnter() {
+    this.isLoading = true;
+    this.isLogin = JSON.parse(localStorage.getItem("isLogin"));
+    this.account = localStorage.getItem("isLoginAs");
+    this.return = this.httpClient.get("http://airqualityapi.herokuapp.com/fetchplaces?username=" + this.account);
+    this.return.subscribe(
+      resp => {
+        if (Object.keys(resp).length == 0) {
+          this.isAdded = false;
+        } else {
+          this.isAdded = true;
+        }
+        this.isLoading = false;
+      })
+  }
+
+  showData(param: string) {
+    var id = param.split(',')[0];
+    var types = param.split(',')[1];
+    this.fetch = this.httpClient.get("http://airqualityapi.herokuapp.com/fetch?q=" + id + "&t=" + types);
+    this.fetch.subscribe(
+      resp => {
+        console.log(resp);
+        var aqi = resp.current.aqi;
+        this.aqi = aqi;
+        if (aqi >= 0 && aqi <= 50) {
+          this.level = "level1";
+        } else if (aqi >= 51 && aqi <= 100) {
+          this.level = "level2";
+        } else if (aqi >= 101 && aqi <= 150) {
+          this.level = "level3";
+        } else if (aqi >= 151 && aqi <= 200) {
+          this.level = "level4";
+        } else if (aqi >= 201 && aqi <= 300) {
+          this.level = "level5";
+        }
+      })
+  }
+
 }
